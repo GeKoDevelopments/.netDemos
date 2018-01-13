d6983d49ef61795a28d85103c9aafb25c0641b92
Geotha <geotha1995@gmail.com>
2018-01-13 21:21:56 +0200

    UWPDemo

---
+++ UWPDemo/UWPdemo/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPdemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            my_text_block.Text = "After Click";
+            Button my_button2 = new Button();
+            my_button2.Content = "Button2";
+            my_button2.HorizontalAlignment = HorizontalAlignment.Left;
+            my_button2.VerticalAlignment = VerticalAlignment.Center;
+            my_grid.Children.Add(my_button2);
+        }
+    }
+}
